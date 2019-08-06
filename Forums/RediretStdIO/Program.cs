609046647290d31b31a140e407f4476fa61457f5
Sam Hobbs <SimpleSamples@users.noreply.github.com>
2019-08-06 09:12:24 -0700

    Add WinFormConsole

---
+++ Forums/RediretStdIO/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace RediretStdIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (Process ChatterBodies = new Process())
+            //{
+            //    ChatterBodies.StartInfo.FileName = exepath;
+            //    ChatterBodies.StartInfo.UseShellExecute = false;
+            //    ChatterBodies.StartInfo.RedirectStandardInput = true;
+            //    ChatterBodies.StartInfo.RedirectStandardOutput = true;
+            //    // ChatterBodies.OutputDataReceived += ChatterBodies_OutputDataReceived;
+            //    ChatterBodies.Start();
+            //    ChatterIn = ChatterBodies.StandardInput.BaseStream;
+            //    ChatterOut = ChatterBodies.StandardOutput.BaseStream;
+            //    //ChatterBodies.BeginOutputReadLine();
+            //    JObject JoMessage = null;
+            //    SendMessage("Hello");
+            //    //DoneEvent.WaitOne();
+            //    //Thread.Sleep(500);
+            //    JoMessage = GetMessage();
+            //    SendMessage("Bye");
+            //    JoMessage = GetMessage();
+            //    //DoneEvent.WaitOne();
+            //}
+            Process process = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "ssh.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+               Arguments = "visitor@localhost"
+            };
+
+            process.StartInfo = psi;
+            try { process.Start(); }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return;
+            }
+            using (System.IO.StreamWriter sw = process.StandardInput)
+            {
+                //sw.WriteLine("ls".Replace("/r/n", "/n"));
+                sw.WriteLine("testssh");
+                sw.WriteLine("dir");
+            }
+
+            process.WaitForExit();
+        }
+    }
+}
---
+++ Forums/WinFormConsole/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// yes or no prompt not working in console's WinForm
+// https://social.msdn.microsoft.com/Forums/en-US/35ac5a44-0658-4d7c-97f6-aa2a303678ac/yes-or-no-prompt-not-working-in-consoles-winform?forum=winforms
+
+namespace WinFormConsole
+{
+    public partial class Form1 : Form
+    {
+        // [return: MarshalAs(UnmanagedType.Bool)]
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int pid);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!AttachConsole(-1))
+            {
+                string s = new Win32Exception(Marshal.GetLastWin32Error()).ToString();
+                MessageBox.Show($"Allocating console; error: {s}");
+                AllocConsole();
+            }
+            // sync all
+            Console.WriteLine("Syncing MaxMon");
+            Console.WriteLine("Please make sure you are connected to internet.");
+            Console.Write("Do you want to Sync all your data now? [y/n] ");
+
+            string risposta = Console.ReadLine();
+
+            if (risposta.Equals("y", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("you choosed YES");
+            }
+            else
+            {
+                Console.WriteLine("you choosed NO");
+            }
+        }
+    }
+}
