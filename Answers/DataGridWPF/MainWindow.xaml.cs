254f2c252e95b09925666f694852493cfb302e76
Sam Hobbs <SimpleSamples@users.noreply.github.com>
2021-08-18 13:38:50 -0700

    First three answers

---
+++ Answers/DataGridWPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionstring = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=G:\Sam\Documents\db1.mdb";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            OleDbConnection Connection = new OleDbConnection(connectionstring);
+            Connection.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM Stores", Connection);
+            DataSet myDataSet = new DataSet();
+            adapter.Fill(myDataSet, "Stores");
+            Grid.DataContext = myDataSet;
+            Grid.ItemsSource = myDataSet.Tables[0].DefaultView;
+        }
+    }
+}
---
+++ Answers/DateDisabled/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+// DateTimePicker in C# windows Form - Microsoft Q&A
+// https://docs.microsoft.com/en-us/answers/questions/518280/datetimepicker-in-c-windows-form.html
+
+namespace DateDisabled
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        IntPtr dtphwnd = IntPtr.Zero;
+        bool CalendarDown = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dtphwnd = dateTimePicker1.Handle;
+            Application.AddMessageFilter(this);
+            this.FormClosed += (s, e) => Application.RemoveMessageFilter(this);
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            const int WM_KEYDOWN = 0x0100;
+            const int WM_KEYUP = 0x0101;
+            if (CalendarDown)
+                return false;
+            if (dtphwnd == IntPtr.Zero)
+            {
+                return false;
+            }
+            if (m.HWnd != dtphwnd)
+            {
+                return false;
+            }
+            if (m.Msg == WM_KEYDOWN || m.Msg == WM_KEYUP)
+                return true;    // stop it from being dispatched
+            return false;
+        }
+
+        private void dateTimePicker1_CloseUp(object sender, EventArgs e)
+        {
+            CalendarDown = false;
+        }
+
+        private void dateTimePicker1_DropDown(object sender, EventArgs e)
+        {
+            CalendarDown = true;
+        }
+
+    }
+}
